fb99cf28dea1f0007137906d3aae0e85a5faa1b2
Torbjørn V Ness <torbness@gmail.com>
2019-10-31 14:25:46 +0100

    Added nice ca spike plot

--- current_conservation_EP/plot_dipoles.py
+++ current_conservation_EP/plot_dipoles.py
@@ -11,11 +11,9 @@ import LFPy
 import neuron
 h = neuron.h
 import matplotlib.pyplot as plt
+from matplotlib.collections import PolyCollection
 from plotting_convention import mark_subplots, simplify_axes
-#from matplotlib.collections import PolyCollection
 
-# from npy_invisible import neuron_models
-# from npy_invisible import plot_lfp
 
 np.random.seed(1234)
 
@@ -44,16 +42,28 @@ def insert_synapses(cell, synparams, section, n, spTimesFun, args, z_min=-1e9, z
         synapses.append(s)
     return synapses
 
+
 def remove_active_mechanisms():
     remove_list = ["Nap_Et2", "NaTa_t", "NaTs2_t", "SKv3_1",
-                                    "SK_E2", "K_Tst", "K_Pst", "Im", "Ih",
-                                    "CaDynamics_E2", "Ca_LVAst", "Ca"]
+                   "SK_E2", "K_Tst", "K_Pst", "Im", "Ih",
+                   "CaDynamics_E2", "Ca_LVAst", "Ca", "Ca_HVA"]
     mt = h.MechanismType(0)
     for sec in h.allsec():
         for seg in sec:
             for mech in remove_list:
                 mt.select(mech)
                 mt.remove()
+
+
+def remove_ca_mechanisms():
+    remove_list = ["SK_E2", "CaDynamics_E2", "Ca_LVAst", "Ca", "Ca_HVA"]
+    mt = h.MechanismType(0)
+    for sec in h.allsec():
+        for seg in sec:
+            for mech in remove_list:
+                mt.select(mech)
+                mt.remove()
+
 
 def make_cell_uniform(Vrest=-70):
     """
@@ -73,34 +83,43 @@ def make_cell_uniform(Vrest=-70):
                 seg.e_pas = seg.e_pas + seg.ica/seg.g_pas
             if neuron.h.ismembrane("Ih"):
                 seg.e_pas += seg.ihcn_Ih/seg.g_pas
-            if neuron.h.ismembrane("Ih_frozen"):
-                seg.e_pas += seg.ihcn_Ih_frozen/seg.g_pas
 
-def return_cell(cell_name):
+
+def return_cell(cell_name, sim_name):
 
     if cell_name == "hay":
+
         neuron.load_mechanisms(join('L5bPCmodelsEH', "mod"))
-        ##define cell parameters used as input to cell-class
         cell_parameters = {
-            'morphology'    : join('L5bPCmodelsEH/morphologies/cell1.asc'),
-            'templatefile'  : [join('L5bPCmodelsEH/models/L5PCbiophys3.hoc'),
+            'morphology': join('L5bPCmodelsEH/morphologies/cell1.asc'),
+            'templatefile': [join('L5bPCmodelsEH/models/L5PCbiophys3.hoc'),
                                join('L5bPCmodelsEH/models/L5PCtemplate.hoc')],
-            'templatename'  : 'L5PCtemplate',
-            'templateargs'  : join('L5bPCmodelsEH/morphologies/cell1.asc'),
-            'passive' : False,
-            'nsegs_method' : None,
-            'dt' : 2**-6,
-            'tstart' : -500,
-            'tstop' : 50,
-            'v_init' : -70,
+            'templatename': 'L5PCtemplate',
+            'templateargs': join('L5bPCmodelsEH/morphologies/cell1.asc'),
+            'passive': False,
+            'nsegs_method': None,
+            'dt': 2**-6,
+            'tstart': -150,
+            'tstop': 10,
+            'v_init': -70,
             'celsius': 34,
-            'pt3d' : True,
+            'pt3d': True,
         }
 
         cell = LFPy.TemplateCell(**cell_parameters)
-        #remove_active_mechanisms()
+
+        if "without_ca" in sim_name:
+            remove_ca_mechanisms()
+            make_cell_uniform(Vrest=-70)
+            print("Removed calcium currents")
+        elif "passive" in sim_name:
+            remove_active_mechanisms()
+            make_cell_uniform(Vrest=-70)
+            print("Removed all active currents")
+        else:
+            print("Control simulation")
+            make_cell_uniform(Vrest=-70)
         cell.set_rotation(x=4.729, y=-3.166)
-        # make_cell_uniform(Vrest=-70)
 
 
     elif cell_name == "two_comp":
@@ -153,14 +172,13 @@ def return_cell(cell_name):
         cell_params = {          # various cell parameters,
                     'morphology': h.all,
                     'delete_sections': False,
-                    'v_init' : -70.,    # initial crossmembrane potential
-                    'passive' : False,   # switch on passive mechs
-                    'nsegs_method' : None,
-                    'lambda_f' : 1000.,
-                    'dt' : dt,   # [ms] dt's should be in powers of 2 for both,
-                    'tstart' : 0.,    # start time of simulation, recorders start at t=0
-                    'tstop' : 10.,   # stop simulation at 200 ms. These can be overridden
-                                        # by setting these arguments i cell.simulation()
+                    'v_init': -70.,    # initial crossmembrane potential
+                    'passive': False,   # switch on passive mechs
+                    'nsegs_method': None,
+                    'lambda_f': 1000.,
+                    'dt': dt,   # [ms] dt's should be in powers of 2 for both,
+                    'tstart': 0.,    # start time of simulation, recorders start at t=0
+                    'tstop': 10.,
                     "pt3d": True,
                 }
 
@@ -324,18 +342,18 @@ def plot_one_LFP_timestep(cell, grid_LFP, laminar_LFP, time_idx, grid_x, grid_z,
     plt.savefig(join(savefolder, 'lfp_{}_{:04d}.png'.format(sim_name, time_idx)), dpi=300)
 
 
-def plot_EAP(cell, grid_LFP, laminar_LFP, time_idx, grid_x, grid_z,
+def plot_EAP(cell, grid_LFP, laminar_LFP, grid_x, grid_z,
                           laminar_elec_params, sim_name, savefolder):
 
+    lam_elec_clr = ['pink', 'cyan']
+    plot_apic = False
 
-    lam_elec_clr =  ['pink', 'cyan']
+
 
     soma_clr = 'orange'
     apic_clr = "olive"
     apic_idx = cell.get_closest_idx(x=0, y=0, z=600)
-    time_idx = np.argmax(np.abs(cell.vmem[0, :] - cell.vmem[0, 0])) if time_idx is None else time_idx
 
-    grid_LFP = grid_LFP[:, time_idx].reshape(grid_x.shape)
 
     plt.close("all")
     fig = plt.figure(figsize=[3, 4])
@@ -343,15 +361,14 @@ def plot_EAP(cell, grid_LFP, laminar_LFP, time_idx, grid_x, grid_z,
     ax = fig.add_axes([.01, .12, .6, .98], xticks=[], yticks=[],
                       aspect=1, frameon=False, )
     # title="LFP at t = {:2.02f} ms".format(cell.tvec[time_idx]))
-    cax = fig.add_axes([0.15, 0.11, 0.3, 0.01], frameon=False)
+    cax = fig.add_axes([0.05, 0.11, 0.4, 0.01], frameon=False)
 
-    ax_vm = fig.add_axes([.67, .13, .25, .27], title=r'membrane\npotential',
+    ax_vm = fig.add_axes([.67, .13, .25, .27], title='membrane\npotential', #ylim=[-80, 50],
                          xlabel="Time (ms)")
-    ax_lam = fig.add_axes([0.67, 0.57, 0.25, 0.27], title="extracellular\npotentials")
+    ax_lam = fig.add_axes([0.67, 0.57, 0.25, 0.27], title="extracellular\npotentials",) #ylim=[-150, 50])
 
     ax_vm.set_ylabel('mV', labelpad=-6)
     ax_lam.set_ylabel('$\mu$V', labelpad=-6)
-
 
     for elec_idx in range(laminar_LFP.shape[0]):
         c = lam_elec_clr[elec_idx]
@@ -365,23 +382,28 @@ def plot_EAP(cell, grid_LFP, laminar_LFP, time_idx, grid_x, grid_z,
         ax.plot([cell.xstart[idx], cell.xend[idx]],
                 [cell.zstart[idx], cell.zend[idx]], lw=1, c='gray')
 
-    ax.plot([250, 350], [-300, -300], 'k', lw=2, clip_on=False)
-    ax.text(300, -350, r'100 $\mu$m', va='center', ha='center')
+    ax.plot([150, 250], [-300, -300], 'k', lw=2, clip_on=False)
+    ax.text(200, -350, r'100 $\mu$m', va='center', ha='center')
 
     ax.plot(cell.xmid[cell.somaidx[0]], cell.zmid[cell.somaidx[0]], 'D', ms=5,
            mec='k', mfc=soma_clr)
-    ax.plot(cell.xmid[apic_idx], cell.zmid[apic_idx], 'D', ms=5,
-           mec='k', mfc=apic_clr)
 
     ax_vm.plot(cell.tvec, cell.vmem[cell.somaidx[0], :], c=soma_clr)
-    ax_vm.plot(cell.tvec, cell.vmem[apic_idx, :], c=apic_clr)
 
-    ax_vm.axvline(cell.tvec[time_idx], ls='--', c='gray')
-    ax_lam.axvline(cell.tvec[time_idx], ls='--', c='gray')
+    if plot_apic:
+        ax.plot(cell.xmid[apic_idx], cell.zmid[apic_idx], 'D', ms=5,
+               mec='k', mfc=apic_clr)
+
+        ax_vm.plot(cell.tvec, cell.vmem[apic_idx, :], c=apic_clr)
+
+    time_idx = 0
+
+    l1 = ax_vm.axvline(cell.tvec[time_idx], ls='--', c='gray')
+    l2 = ax_lam.axvline(cell.tvec[time_idx], ls='--', c='gray')
 
     num = 15
-    levels = np.logspace(-2, 0, num=num)
-    scale_max = 100.  # 10**np.ceil(np.log10(np.max(np.abs(LFP)))) / 10
+    levels = np.logspace(-2.3, 0, num=num)
+    scale_max = 10 # 100.
 
     levels_norm = scale_max * np.concatenate((-levels[::-1], levels))
     # rainbow_cmap = plt.cm.get_cmap('PRGn') # rainbow, spectral, RdYlBu
@@ -392,42 +414,116 @@ def plot_EAP(cell, grid_LFP, laminar_LFP, time_idx, grid_x, grid_z,
 
     # ticks = [levels_norm[2*i] for i in range(int(num/2) + 1)] + [levels_norm[num + 2*i] for i in range(int(num/2) + 1)]
 
-    ep_intervals = ax.contourf(grid_x, grid_z, grid_LFP,
-                               zorder=-2, colors=colors_from_map,
-                               levels=levels_norm, extend='both')
+    from mpi4py import MPI
 
-    ax.contour(grid_x, grid_z, grid_LFP, colors='k', linewidths=(1), zorder=-2,
-               levels=levels_norm)
+    # MPI stuff we're using
+    COMM = MPI.COMM_WORLD
+    SIZE = COMM.Get_size()
+    RANK = COMM.Get_rank()
 
-    cbar = fig.colorbar(ep_intervals, cax=cax, orientation='horizontal',
-                        format='%.0E', extend='max')
+    for time_idx in range(len(cell.tvec))[0::10]:
+        if not divmod(time_idx, SIZE)[1] == RANK:
+            continue
+        # time_idx = np.argmax(np.abs(cell.vmem[0, :] - cell.vmem[0, 0])) if time_idx is None else time_idx
+        l1.set_xdata(cell.tvec[time_idx])
+        l2.set_xdata(cell.tvec[time_idx])
 
-    cbar.set_ticks([-100, -10,  0, 10, 100])
-    cax.set_xticklabels([-100, -10, 0, 10, 100],
-                        fontsize=8, rotation=45)
-    cbar.set_label('$\phi$ ($\mu$V)', labelpad=-5)
+        grid_LFP_ = grid_LFP[:, time_idx].reshape(grid_x.shape)
 
-    simplify_axes([ax_vm, ax_lam])
+        if divmod(time_idx, SIZE)[1] == RANK:
+            print(time_idx)
 
-    plt.savefig(join(savefolder, 'eap_{}_{:04d}.png'.format(sim_name, time_idx)), dpi=150)
+        ep_intervals = ax.contourf(grid_x, grid_z, grid_LFP_,
+                                   zorder=-2, colors=colors_from_map,
+                                   levels=levels_norm, extend='both')
+
+        ax.contour(grid_x, grid_z, grid_LFP_, colors='k', linewidths=(1), zorder=-2,
+                   levels=levels_norm)
+
+        cbar = fig.colorbar(ep_intervals, cax=cax, orientation='horizontal',
+                            format='%.0E', extend='max')
+
+        cbar.set_ticks(np.array([-1, -0.1,  0,  0.1, 1]) * scale_max)
+        cax.set_xticklabels(np.array(np.array([-1, -0.1, 0, 0.1, 1]) * scale_max, dtype=int),
+                            fontsize=7, rotation=45)
+        cbar.set_label('$\phi$ ($\mu$V)', labelpad=-5)
+
+        simplify_axes([ax_vm, ax_lam])
+
+        plt.savefig(join(savefolder, 'eap_{}_{:04d}.png'.format(sim_name, time_idx)), dpi=150)
+
+
+def plot_spatial_time_traces(cell, grid_LFP, grid_elec_params,
+                          sim_name, savefolder):
+
+    plt.close("all")
+    fig = plt.figure(figsize=[5, 4])
+
+    ax = fig.add_axes([.01, .1, .6, .85], xticks=[], yticks=[],
+                      aspect=1, frameon=False, xlim=[-150, 150], ylim=[-100, 300])
+
+    ax_vm = fig.add_axes([.75, .2, .2, .6], title='somatic\nmembrane\npotential', #ylim=[-80, 50],
+                         xlabel="Time (ms)")
+
+    ax_vm.set_ylabel('mV', labelpad=-1)
+
+    dx = np.diff(sorted(set(grid_electrode.x)))[0]
+    dz = grid_electrode.z[1] - grid_electrode.z[0]
+    sig_shrink_factor = 0.7
+
+    ep_sig_scale = 100
+
+    norm_LFP = dz / ep_sig_scale
+
+    print(np.max(np.abs(grid_LFP)))
+
+    cell_clr = "0.8"
+
+    #plot morphology
+    zips = []
+    for x, z in cell.get_idx_polygons():
+        zips.append(list(zip(x, z)))
+    polycol = PolyCollection(zips,
+                             edgecolors=cell_clr, lw=0.5,# alpha=0.2,
+                             facecolors=cell_clr)
+    ax.add_collection(polycol)
+    ax.plot(cell.somapos[0], cell.somapos[1], 'o', c=cell_clr, ms=30)
+
+    for elec_idx in range(grid_LFP.shape[0]):
+        x = grid_elec_params['x'][elec_idx]
+        z = grid_elec_params['z'][elec_idx]
+
+        t_ = cell.tvec / cell.tvec[-1] * dx * sig_shrink_factor + x
+        y_sig = grid_LFP[elec_idx] * norm_LFP * sig_shrink_factor + z
+
+        ax.plot(t_, y_sig, c='k', clip_on=False)
+        ax.plot(x, z, '.', c='k', zorder=100, clip_on=False)
+
+    ax.plot([25, 75], [-110, -110], cell_clr, lw=2, clip_on=False)
+    ax.text(50, -120, r'50 $\mu$m', va='center', ha='center', color=cell_clr)
+
+    ax.plot([-75, -75 + dx * sig_shrink_factor], [-110, -110], 'k', lw=2, clip_on=False)
+    ax.text(-95, -120, r'10 ms', va='center', ha='left', color='k')
+
+    ax.plot([-75 + dx * sig_shrink_factor, -75 + dx * sig_shrink_factor],
+            [-110, -110- dz * sig_shrink_factor], 'k', lw=2, clip_on=False)
+    ax.text(-35, -130, r'{:d} $\mu$V'.format(ep_sig_scale), va='center', ha='left', color='k')
+
+    ax_vm.plot(cell.tvec, cell.vmem[cell.somaidx[0], :], c='k')
+    simplify_axes([ax_vm])
+
+    plt.savefig(join(savefolder, 'traces_{}.png'.format(sim_name)), dpi=150)
 
 
 synapse_type = ["single_tuft", "single_soma"][1]
 cell_name = ["two_comp", "hay"][1]
 
-sim_name = '{}_{}'.format(cell_name, synapse_type)
-savefolder = join(cell_name, sim_name)
+sim_name = '{}_{}_{}_traces'.format(cell_name, synapse_type, "spike")
+savefolder = join(sim_name)
 
-if not os.path.isdir(savefolder):
-    os.makedirs(savefolder)
+os.makedirs(savefolder, exist_ok=True)
 
-# depth = 40
-# depth = 165
-# depth = 600
-
-
-cell = return_cell(cell_name)
-
+cell = return_cell(cell_name, sim_name)
 
 # Define synapse parameters
 synapse_params = {
@@ -439,31 +535,20 @@ synapse_params = {
     'record_current' : True,    # record synapse current
 }
 
-# insert_synapses_GABA_A_args = {
-#     'section' : 'allsec',
-#     'z_min': np.max(cell.zend) - depth,
-#     'n' : 100,
-#     'spTimesFun' : np.random.normal,#LFPy.inputgenerators.get_activation_times_from_distribution,
-#     'args' : dict(size=1, #tstart=50, tstop=60,
-#                   # distribution=scipy.stats.norm,
-#                   # rvs_args=dict(loc=50, scale=0.001)
-#                   loc=20,
-#                   scale=2,
-#                   )
-# }
-
 if "soma" in synapse_type:
     synapse_params["idx"] = cell.get_closest_idx(x=0, z=0)
 elif "tuft" in synapse_type:
     synapse_params["idx"] = cell.get_closest_idx(x=100, z=1400)
 
-synapse_params["weight"] = 0.1
+synapse_params["weight"] = 0.05#0.2
 synapses = [LFPy.Synapse(cell, **synapse_params)]
-synapses[0].set_spike_times(np.array([1., 5., .8]))
-
+# synapses[0].set_spike_times(np.array([1., 5., 9.]))
+synapses[0].set_spike_times(np.array([1.]))
 
 # Create a grid of measurement locations, in (mum)
-grid_x, grid_z = np.mgrid[-400:401:25, -400:1401:25]
+# grid_x, grid_z = np.mgrid[-550:551:25, -600:1501:25]
+# grid_x, grid_z = np.mgrid[-75:76:50, -75:76:50]
+grid_x, grid_z = np.mgrid[-75:76:50, -75:251:50]
 grid_y = np.zeros(grid_x.shape)
 
 # Define electrode parameters
@@ -473,7 +558,6 @@ grid_elec_params = {
     'y': grid_y.flatten(),
     'z': grid_z.flatten()
 }
-
 
 # Create a grid of measurement locations, in (mum)
 laminar_z = np.array([0, 200])
@@ -487,14 +571,9 @@ laminar_elec_params = {
     'z': laminar_z
 }
 
-
 # Run simulation, electrode object argument in cell.simulate
 print("running simulation...")
 cell.simulate(rec_imem=True, rec_vmem=True)
-
-# [plt.plot(cell.tvec, cell.imem[idx, :]) for idx in range(cell.totnsegs)]
-# plt.savefig("test.png")
-# plt.show()
 
 laminar_electrode = LFPy.RecExtElectrode(cell, **laminar_elec_params)
 laminar_electrode.calc_lfp()
@@ -511,10 +590,12 @@ max_elec, max_time_idx = np.unravel_index(np.abs(laminar_LFP).argmax(), grid_LFP
 # plot_EAP(cell, grid_LFP, laminar_LFP, None,
 #                             grid_x, grid_z, laminar_elec_params,  sim_name, savefolder)
 
-for time_idx in range(len(cell.tvec))[0::1]:
-    plot_EAP(cell, grid_LFP, laminar_LFP, time_idx,
-             grid_x, grid_z, laminar_elec_params, sim_name, savefolder)
+plot_spatial_time_traces(cell, grid_LFP, grid_elec_params,  sim_name, savefolder)
+
+# plot_EAP(cell, grid_LFP, laminar_LFP,
+#                  grid_x, grid_z, laminar_elec_params, sim_name, savefolder)
 # #     print(time_idx)
 #     plot_one_LFP_timestep(cell, grid_LFP, laminar_LFP, time_idx,
 #                            grid_x, grid_z, laminar_elec_params, synapses,
 #                            synapse_params, sim_name, savefolder)
+#     sys.exit()
